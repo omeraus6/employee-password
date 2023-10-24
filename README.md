58358c5f64977a58f5c52e7665705f9cb4f0782c
omeraus6 <omeraus6@yahoo.com>
2023-10-23 22:11:15 -0700

    update readme and javascript, add images

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# employee-password
+# Employee-Password-generator
+
+## Description
+
+- What was your motivation? I learn alot about javascript and how make my website more professional pages 
+- Why did you build this project? I build this project to generate new password for employee or users
+- What problem does it solve?
+1- I solved many problems like: make sure password length between 8-128 characters and should not accept number less than 8 or greater than 128 and doesn't accept string.
+2- make sure the password contain at least one of the characters types Uppercase letter,lowercase letter,Numbers and special characters 
+3- shows message to the user to include which type of characters they needs to generate password
+4- show random password with the characters type they need.
+- What did you learn? I learn javascript and how to used function,object,array and how to used the elements inside the HTML and  Class or attribute on CSS.
+
+## Installation
+
+N/A
+
+## Usage
+when open the webpage, there is a Botton called "Generate Password" , when click it will show the help Guide message for users and employee to know how to generate password, after that it will show a input message box ask user for password lenght between 8-128 characters, and then ask for characters type uppercase,lowercase,number and special characters 
+finaly it will show the new password genarate inside the textbox in a webpage
+I include some pictures for my code and webpage run.
+
+
+
+
+![alt text](assets/images/1.png)
+image 1: four characters variable array
+
+![alt text](assets/images/2.png)
+image 2: one variable array for message box and some reset values variables
+
+![alt text](assets/images/3.png)
+image 3:  generatepassword function and it is connect to two function image 4 and 5
+
+![alt text](assets/images/4.png)
+image 4: passwordRequired function
+
+![alt text](assets/images/5.png)
+image 5: collectpassReq function
+
+![alt text](assets/images/6.png)
+image 6: generate random array and its connect from image 5 to image 6 and then image 6 will return the result to image 5
+
+![alt text](assets/images/10.png)
+image 10: webpage - homepage
+
+![alt text](assets/images/11.png)
+image 11: when click to the bottom show this guide message
+
+![alt text](assets/images/12.png)
+image 12: message box for enter password length
+
+![alt text](assets/images/13.png)
+image 13: show this message if the number was less than 8 or greater than 128 or typing letters
+
+![alt text](assets/images/14.png)
+image 14: message show inside the textbox to click on the bottom again to generate password
+
+![alt text](assets/images/15.png)
+image 15: message show  to add upercase,lowercase,number and/or special characters
+
+![alt text](assets/images/16.png)
+image 16: message show  when click "no" if you don't add one of the characters types
+
+![alt text](assets/images/17.png)
+image 17: this message show  when no characters types accept it
+
+![alt text](assets/images/18.png)
+image 18: password-generated has (20characters,uppercase,lowercase,numbers)
+
+![alt text](assets/images/19.png)
+image 19: password-generated has (20characters,uppercase,lowercase,numbers and special characters)
+
+
+## Credits
+
+Thank you for tips and sugestion from Bootcamp instructors and classmates, I hava used https://www.w3schools.com/ , https://stackoverflow.com/ and https://developer.mozilla.org/en-US/docs/Web/CSS/grid-template to reserch information 
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
 // Assignment code here
 
+// Created four variables array for UpperCaseLetter,LowerCaseLetter,Numbers and SpecialCharacters   
+var abcupper= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var abclower =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numeric=[0,1,2,3,4,5,6,7,8,9];
+var specialchar =[' ','!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?',']',"[","",'@','^','_','`','{','|','}' ,'~'];
+// I remove "\" from special characters
+
+// Create array for MessageBox Dialog to show the message for every Character Array   
+var message = ["Do like to add uppercase letter in you password?","Do like to add lowercase letter to your password?",
+              "Do you like to add numbers in your password?","Do you like to add special charctes in your password?"];
+
+// Created two arrays and one variable to save the status for each array if it's chosen 
+var valuearray= [0,0,0,0];
+var count = 0;
+var functchar= [abcupper,abclower,numeric,specialchar]
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,5 +31,119 @@ function writePassword() {
 
 }
 
+// This function generatepassword is connected to another tow functions passwordRiquarment and collectpassReq
+function generatePassword()
+{
+  alert("          Welcom to Employee Password Generater Guide\nPlease follow all requirements" +
+        "\n\tFirst: Passwword length should be between 8-128 chararcters\n\tSecond: Accept at least one of " +
+        "the following password characters types :\n\t\tUppercase Letters,Lowercase Letters,Numbers and Special charcters");
+  // Reset value
+  valuearray=[0,0,0,0];
+  count=0;
+  var finalpassword = "";
+
+  // generate password length between 8-128 characters and has validation if number less than 8 , grater than 128 or string 
+  var passwordlen = prompt("Enter your password Length between 8-128 characters: ", "between 8-128 characters");
+  if (passwordlen<8 || passwordlen>128 || isNaN(passwordlen))
+  {
+    alert("Password length should be between 8-128 characters");
+    finalpassword = "Click Generate password bottom to start again"
+    return finalpassword;
+  }
+  else 
+  {
+    
+    passwordRequired();
+    var y= collectpassReq(passwordlen);
+
+    //this forloop generate final Password and return the new password to writepawwword function
+    for(var i=0;i<y.length;i++)
+    {
+       var index = Math.floor(Math.random()*y.length);
+       finalpassword = finalpassword + y[index];
+    }
+   
+  }
+
+  return finalpassword;
+}
+
+
+// check the password question requirment if its uppercase,lowercase,number and special character 
+// this function can be update it because has array for question dielog box and valuearray as a check box accept or not
+function passwordRequired()
+{
+  for(var i=0;i<valuearray.length;i++)
+  {
+    var abcu = confirm(message[i]);
+    if(abcu == true)
+    {
+      valuearray[i]=1;
+      count++
+    }
+    else
+    {
+      alert("is not include in your password");
+    }
+  }
+      
+  return;
+}
+
+// These next tow function , the first one: starting to generate the final password from the all requirment choosen
+// Second function: generate random password characters for all requirments, then function send back the new random array to 
+// generatePassword function
+function collectpassReq(passlength)
+{
+  // x array is an array collecting all choosen characters on one array 
+  var x=[];  
+
+  var integernum = Math.floor(passlength/count);
+  var remainum = passlength%count;
+
+  if(count==0)
+  {
+    alert("No password generate because you need Accept at least one of " +
+    "the following password characters types\n\tUppercase Letters,Lowercase Letters,Numbers and Special charcters");
+  }
+
+  for(var i=0;i<valuearray.length;i++)
+  {
+    if(valuearray[i]==1 && count != 1)
+    {
+      x= x.concat(collectrandomchar(i,integernum,0));
+      count--;
+    }
+    else if( valuearray[i]==1 && count==1)
+    {
+      x=x.concat(collectrandomchar(i,integernum,remainum));
+      count--;
+    }
+  }
+
+  return x;
+}
+
+
+function collectrandomchar(valueindex,integernum,remainum)
+{
+  
+  var randomspecial = [];
+  var arrayneed = functchar[valueindex];
+
+  for(var i=0;i<(integernum+remainum);i++)
+  {
+    var index = Math.floor(Math.random()*arrayneed.length);
+    randomspecial[i] = arrayneed[index];
+  }
+
+  return randomspecial;
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generatePassword() need to buil this function to generate password
+//passwordText the password holder area to shows in textarea
+//passwordText.value = password show this password after generate and put it in textarea
